b91832b5dfb3964de98fb814014051e426633643
Himanshi Singh <152288666+Himanshitweety@users.noreply.github.com>
2026-08-17 18:45:14 +0000

    Added the Dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
+import pandas as pd
+import numpy as np
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Loaded the Data
+df = pd.read_csv('/workspaces/Global-Demographics-And-Market-Intelligence-Portal/world_population_by_country_2026.csv')
